6b9ed056c8d4b480b6bc9f4f5b68fca0842f031e
kaxuna1 <kaxgel11@gmail.com>
2013-10-10 14:07:36 +0400

    ll

---
+++ Java Gui practice/src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kakha
+ * Date: 10/10/13
+ * Time: 2:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    public static void main() {
+        System.out.println("kaxa");
+    }
+}
